4055ae19921231dce0179c2b9dc35f5136267148
Katarzyna Bulat <kasia.bulat@gmail.com>
2016-06-02 16:16:47 +0200

    wykroczenia - więcej informacji

--- src/controllers/OffencesController.java
+++ src/controllers/OffencesController.java
@@ -2,8 +2,14 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class OffencesController {
 	//TODO: add fields
@@ -21,7 +27,16 @@ public class OffencesController {
 	}
 	@FXML
 	public void showMoreInfo (final ActionEvent event) {
-		// TODO: show more info basing on all three tables - mandaty_wystawiający, wykroczenia, mandaty
+		try {
+			final Parent root= FXMLLoader.load(getClass().getResource("../FXML/offencesMoreInfoWindow.fxml"));
+			final Stage stage=new Stage();
+			stage.setTitle("Wykroczenia - szczegółowe informacje");
+			stage.setScene(new Scene(root,450,310));
+			stage.show();
+
+		} catch(final IOException e) {
+			e.printStackTrace();
+		}
 	}
 	@FXML
 	public void showStatistics (final ActionEvent event) {
